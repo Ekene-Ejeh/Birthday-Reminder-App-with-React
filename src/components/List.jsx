1f8f2d767b733432500e391200ccae99c15b6687
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-08 12:18:01 -0700

    created board.css for styling and List component

---
+++ src/components/List.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function List() {
+	return (
+		<ul>
+			<li>
+				<div className="flex">
+					<img src="" alt="img" />
+					<div className="tittle">
+						<h3 className="name">John Doe</h3>
+						<h5 className="age">20 years</h5>
+					</div>
+				</div>
+			</li>
+		</ul>
+	);
+}
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./Board.css";
+import List from "./List";
 
 export default function board() {
 	return (
 		<main id="site-main">
-			<h1 className="text-dark title">Birthday Reminder Component</h1>
+			<h1 className="text-dark title">Birthday Reminder</h1>
+
+			<div className="board">
+				<List />
+			</div>
 		</main>
 	);
 }
