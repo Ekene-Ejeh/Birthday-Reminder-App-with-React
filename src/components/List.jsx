7ade861f5a1edb5005273682790e80fa7aee4cbc
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-08 13:16:34 -0700

    added data for the birthday reminder containg img, name and date of birth on the Board component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,7 +5,10 @@ export default function List() {
 		<ul>
 			<li>
 				<div className="flex">
-					<img src="" alt="img" />
+					<img
+						src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+						alt="img"
+					/>
 					<div className="tittle">
 						<h3 className="name">John Doe</h3>
 						<h5 className="age">20 years</h5>
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -2,6 +2,39 @@ import React from "react";
 import "./Board.css";
 import List from "./List";
 
+const Info = [
+	{
+		img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+		name: "Sofia Cooper",
+		birthday: "1995-01-31",
+	},
+	{
+		img: "https://images.unsplash.com/photo-1503185912284-5271ff81b9a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+		name: "Miller Wright",
+		birthday: "1998-05-29",
+	},
+	{
+		img: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+		name: "Tara Kelly",
+		birthday: "1991-02-02",
+	},
+	{
+		img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+		name: "Freddie Watson",
+		birthday: "1999-10-15",
+	},
+	{
+		img: "https://images.unsplash.com/photo-1503185912284-5271ff81b9a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+		name: "Brianna Baker",
+		birthday: "1994-07-05",
+	},
+	{
+		img: "https://images.unsplash.com/photo-1503185912284-5271ff81b9a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+		name: "Brad Myers",
+		birthday: "1997-01-28",
+	},
+];
+
 export default function board() {
 	return (
 		<main id="site-main">
